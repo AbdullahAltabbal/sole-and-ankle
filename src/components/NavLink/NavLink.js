65d4e7a1b67e0165e86b6d367803458ed3cbf5c2
Abdullah Al Tabbal <xlp1abode@gmail.com>
2022-05-08 00:18:40 +0200

    fix bug

--- src/components/NavLink/NavLink.js
+++ src/components/NavLink/NavLink.js
@@ -4,6 +4,24 @@ import styled from 'styled-components';
 import { WEIGHTS } from '../../constants';
 
 
+const Wrapper = styled.a`
+  position: relative;
+  display: block;
+  font-size: 1.125rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: var(--color-gray-900);
+  font-weight: ${WEIGHTS.medium};
+
+  /*
+    Text slide-up effect
+  */
+  overflow: hidden;
+  &:first-of-type {
+    color: var(--color-secondary);
+  }
+`;
+
 const Text = styled.span`
   display: block;
   transform: translateY(var(--translate-from));
@@ -41,23 +59,5 @@ const NavLink = ({ children, ...delegated }) => {
         </Wrapper>
     );
 };
-
-const Wrapper = styled.a`
-  position: relative;
-  display: block;
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: var(--color-gray-900);
-  font-weight: ${WEIGHTS.medium};
-
-  /*
-    Text slide-up effect
-  */
-  overflow: hidden;
-  &:first-of-type {
-    color: var(--color-secondary);
-  }
-`;
 
 export default NavLink;
