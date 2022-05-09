2f23caee3f645fdfdfc8b59ac8f72ec879a73092
Abdullah Al Tabbal <xlp1abode@gmail.com>
2022-05-09 15:47:25 +0200

    add break_points and queries

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,26 @@
+export const BREAK_POINTS = {
+  phone: 600,
+  tablet: 950,
+  laptop: 1300,
+};
+
+export const QUERIES = {
+  phoneAndSmaller: `(max-width: ${BREAK_POINTS.phone / 16}rem)`,
+  tabletAndSmaller: `(max-width: ${BREAK_POINTS.tablet / 16}rem)`,
+  laptop: `(max-width: ${BREAK_POINTS.laptop / 16}rem)`,
+};
+
 export const COLORS = {
-  white: 'hsl(0deg, 0%, 100%)',
+  white: "hsl(0deg, 0%, 100%)",
   gray: {
-    100: 'hsl(185deg, 5%, 95%)',
-    300: 'hsl(190deg, 5%, 80%)',
-    500: 'hsl(196deg, 4%, 60%)',
-    700: 'hsl(220deg, 5%, 40%)',
-    900: 'hsl(220deg, 3%, 20%)',
+    100: "hsl(185deg, 5%, 95%)",
+    300: "hsl(190deg, 5%, 80%)",
+    500: "hsl(196deg, 4%, 60%)",
+    700: "hsl(220deg, 5%, 40%)",
+    900: "hsl(220deg, 3%, 20%)",
   },
-  primary: 'hsl(340deg, 65%, 47%)',
-  secondary: 'hsl(240deg, 60%, 63%)',
+  primary: "hsl(340deg, 65%, 47%)",
+  secondary: "hsl(240deg, 60%, 63%)",
 };
 
 export const WEIGHTS = {
