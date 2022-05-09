7d666a92a22183f507eda1ea4b6cd1df43d18aca
Abdullah Al Tabbal <xlp1abode@gmail.com>
2022-05-09 15:48:24 +0200

    responsive nav, still not perfect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import NavLink from '../NavLink/NavLink';
+import { COLORS, QUERIES } from "../../constants";
+import Logo from "../Logo";
+import Icon from "../Icon";
+import SuperHeader from "../SuperHeader";
+import NavLink from "../NavLink/NavLink";
+import UnstyledButton from "../UnstyledButton";
 
 const Header = () => {
   // Our site features two visual headers, but they should be
@@ -14,16 +16,29 @@ const Header = () => {
       <SuperHeader />
       <MainHeader>
         <Side>
-          <Logo />
+          <LogoWrapper>
+            <Logo />
+          </LogoWrapper>
         </Side>
-        <Nav>
+        <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
-        </Nav>
+        </DesktopNav>
+        <MobileNav>
+          <UnstyledButton>
+            <Icon id="shopping-bag"></Icon>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="search"></Icon>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="menu"></Icon>
+          </UnstyledButton>
+        </MobileNav>
         <Side />
       </MainHeader>
     </header>
@@ -36,18 +51,55 @@ const MainHeader = styled.div`
   height: 72px;
   padding: 8px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media ${QUERIES.tabletAndSmaller} {
+    justify-content: space-between;
+    align-items: center;
+    border-top: 4px solid ${COLORS.gray[900]};
+  }
+  @media ${QUERIES.phoneAndSmaller} {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
 `;
 
 const Side = styled.div`
   flex: 1;
-`
 
-const Nav = styled.nav`
-padding-left:26px;;
-margin: 0 32px;;
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+
+const DesktopNav = styled.nav`
+  padding-left: 26px;
+  margin: 0 32px;
+  display: flex;
+  gap: 32px;
+  padding: 16px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+
+const MobileNav = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    gap: 32px;
     display: flex;
-    gap:32px;
-    padding: 16px; 
+  }
+  @media ${QUERIES.phoneAndSmaller} {
+    gap: 16px;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  flex: 1;
+  @media ${QUERIES.tabletAndSmaller} {
+    flex: revert;
+  }
 `;
 
 export default Header;
