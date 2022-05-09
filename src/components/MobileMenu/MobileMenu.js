b106c409f486f0f8a64f154e00939fe7513a1b7f
Abdullah Al Tabbal <xlp1abode@gmail.com>
2022-05-09 19:19:11 +0200

    panel enter animation

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,58 +1,90 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled, { keyframes } from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-    return (
-        <Overlay isOpen={isOpen} onDismiss={onDismiss}>
-            <Content aria-label="Menu">
-                <CloseButton onClick={onDismiss}>
-                    <Icon id="close" />
-                    <VisuallyHidden>Dismiss menu</VisuallyHidden>
-                </CloseButton>
-                <Filler />
-                <Nav>
-                    <NavLink href="/sale">Sale</NavLink>
-                    <NavLink href="/new">New&nbsp;Releases</NavLink>
-                    <NavLink href="/men">Men</NavLink>
-                    <NavLink href="/women">Women</NavLink>
-                    <NavLink href="/kids">Kids</NavLink>
-                    <NavLink href="/collections">Collections</NavLink>
-                </Nav>
-                <Footer>
-                    <a href="/terms">Terms and Conditions</a>
-                    <a href="/privacy">Privacy Policy</a>
-                    <a href="/contact">Contact Us</a>
-                </Footer>
-            </Content>
-        </Overlay>
-    );
+  return (
+    <Wrapper isOpen={isOpen} onDismiss={onDismiss}>
+      <Backdrop />
+      <Content aria-label="Menu">
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close" />
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+        </CloseButton>
+        <Filler />
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+        <Footer>
+          <a href="/terms">Terms and Conditions</a>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/contact">Contact Us</a>
+        </Footer>
+      </Content>
+    </Wrapper>
+  );
 };
 
-const Overlay = styled(DialogOverlay)`
+const fadeIn = keyframes`
+  from{
+    opacity: 0;
+  }
+  to{
+    opacity: 1;
+  }
+`;
+
+const slideIn = keyframes`
+  from{
+   transform: translateX(100%);
+  }
+  to{
+    transform: translateX(0%);
+  }
+`;
+
+const Wrapper = styled(DialogOverlay)`
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  background: var(--color-backdrop);
+  background: transparent;
   display: flex;
   justify-content: flex-end;
 `;
 
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.5);
+  animation: ${fadeIn} 500ms;
+`;
+
 const Content = styled(DialogContent)`
+  position: relative;
   background: white;
   width: 300px;
   height: 100%;
   padding: 32px;
   display: flex;
   flex-direction: column;
+  animation: ${slideIn} 500ms both;
+  animation-delay: 200ms;
 `;
 
 const CloseButton = styled(UnstyledButton)`
